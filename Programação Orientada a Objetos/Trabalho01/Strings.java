2e8adefc7aa05ad4c7d36a1073cedd009438b6ee
Roger Jacobsen <134503628+RogerJacobsenJR@users.noreply.github.com>
2023-08-31 21:40:45 -0300

    Add files via upload

---
+++ Programação Orientada a Objetos/Trabalho01/Strings.java
@@ -0,0 +1,23 @@
+public class Strings {
+	private String cadFunc = "Dígite 1 para cadastro de funcionario";
+	private String cadCli = "Dígite 2 para cadastro de cliente";
+	public String getCadCli() {
+		return cadCli;
+	}
+	public void setCadCli(String cadCli) {
+		this.cadCli = cadCli;
+	}
+	public String getCadSair() {
+		return cadSair;
+	}
+	public void setCadSair(String cadSair) {
+		this.cadSair = cadSair;
+	}
+	private String cadSair = "Dígite 3 para sair";
+	public String getCadFunc() {
+		return cadFunc;
+	}
+	public void setCadFunc(String cadFunc) {
+		this.cadFunc = cadFunc;
+	}
+}
